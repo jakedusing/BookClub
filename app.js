589b6614aeb0130dbb2654b4c8c3101345b33f66
Jake Dusing <dusingjake@gmail.com>
2024-09-17 20:09:06 -0500

    add more to error handling

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
 const methodOverride = require("method-override");
 const Book = require("./models/book");
+const catchAsync = require("./utils/catchAsync");
 
 mongoose.connect("mongodb://localhost:27017/bookclub");
 
@@ -26,42 +28,60 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/books", async (req, res) => {
-  const books = await Book.find({});
-  res.render("books/index", { books });
-});
+app.get(
+  "/books",
+  catchAsync(async (req, res) => {
+    const books = await Book.find({});
+    res.render("books/index", { books });
+  })
+);
 
 app.get("/books/new", (req, res) => {
   res.render("books/new");
 });
 
-app.post("/books", async (req, res) => {
-  const book = new Book(req.body.book);
-  await book.save();
-  res.redirect(`/books/${book._id}`);
-});
+app.post(
+  "/books",
+  catchAsync(async (req, res, next) => {
+    const book = new Book(req.body.book);
+    await book.save();
+    res.redirect(`/books/${book._id}`);
+  })
+);
 
-app.get("/books/:id", async (req, res) => {
-  const book = await Book.findById(req.params.id);
-  res.render("books/show", { book });
-});
+app.get(
+  "/books/:id",
+  catchAsync(async (req, res) => {
+    const book = await Book.findById(req.params.id);
+    res.render("books/show", { book });
+  })
+);
 
-app.get("/books/:id/edit", async (req, res) => {
-  const book = await Book.findById(req.params.id);
-  res.render("books/edit", { book });
-});
+app.get(
+  "/books/:id/edit",
+  catchAsync(async (req, res) => {
+    const book = await Book.findById(req.params.id);
+    res.render("books/edit", { book });
+  })
+);
 
-app.put("/books/:id", async (req, res) => {
-  const { id } = req.params;
-  const book = await Book.findByIdAndUpdate(id, { ...req.body.book });
-  res.redirect(`/books/${book._id}`);
-});
+app.put(
+  "/books/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const book = await Book.findByIdAndUpdate(id, { ...req.body.book });
+    res.redirect(`/books/${book._id}`);
+  })
+);
 
-app.delete("/books/:id", async (req, res) => {
-  const { id } = req.params;
-  await Book.findByIdAndDelete(id);
-  res.redirect("/books");
-});
+app.delete(
+  "/books/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Book.findByIdAndDelete(id);
+    res.redirect("/books");
+  })
+);
 
 app.use((err, req, res, next) => {
   res.send("Oh boy, something went wrong!");
