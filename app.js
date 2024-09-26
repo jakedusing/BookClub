58aaf47d8f5b02913e4c612aae12e23eca1777de
Jake Dusing <dusingjake@gmail.com>
2024-09-26 16:19:50 -0500

    restructure review routes

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const Joi = require("joi");
-const { bookSchema, reviewSchema } = require("./schemas");
-const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
-const Book = require("./models/book");
-const Review = require("./models/review");
 
 const books = require("./routes/books");
+const reviews = require("./routes/reviews");
 
 mongoose.connect("mongodb://localhost:27017/bookclub");
 
@@ -30,43 +26,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 app.use("/books", books);
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
+app.use("/books/:id/reviews", reviews);
 
 app.get("/", (req, res) => {
   res.render("home");
 });
-
-app.post(
-  "/books/:id/reviews",
-  validateReview,
-  catchAsync(async (req, res) => {
-    const book = await Book.findById(req.params.id);
-    const review = new Review(req.body.review);
-    book.reviews.push(review);
-    await review.save();
-    await book.save();
-    res.redirect(`/books/${book._id}`);
-  })
-);
-
-app.delete(
-  "/books/:id/reviews/:reviewId",
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Book.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/books/${id}`);
-  })
-);
 
 app.all(/(.*)/, (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
