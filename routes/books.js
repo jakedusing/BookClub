bc438f4980d0f27eadc158e38d210d6e4558e201
Jake Dusing <dusingjake@gmail.com>
2024-10-15 18:24:59 -0500

    add multer

--- routes/books.js
+++ routes/books.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const books = require("../controllers/books");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isUser, validateBook } = require("../middleware");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 const Book = require("../models/book");
 
 router
   .route("/")
   .get(catchAsync(books.index))
-  .post(isLoggedIn, validateBook, catchAsync(books.createBook));
+  // .post(isLoggedIn, validateBook, catchAsync(books.createBook));
+  .post(upload.array("image"), (req, res) => {
+    res.send(req.body, req.files);
+  });
 
 router.get("/new", isLoggedIn, books.renderNewForm);
 
