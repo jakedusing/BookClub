7b376f9a468e35961cf71e5bd37ae4a9c3cfc71f
Jake Dusing <dusingjake@gmail.com>
2024-10-18 20:39:23 -0500

    update styling and add random img for login and register

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,21 @@
 const User = require("../models/user");
 
+const links = [
+  "https://images.unsplash.com/photo-1508169351866-777fc0047ac5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1533669955142-6a73332af4db?q=80&w=1948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1475243907012-e01b4e4b0a1b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1506880018603-83d5b814b5a6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1414124488080-0188dcbb8834?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+function getRandomLink() {
+  const randomIndex = Math.floor(Math.random() * links.length);
+  return links[randomIndex];
+}
+
 module.exports.renderRegister = (req, res) => {
-  res.render("users/register");
+  const randomLink = getRandomLink();
+  res.render("users/register", { randomLink });
 };
 
 module.exports.register = async (req, res) => {
@@ -21,7 +35,8 @@ module.exports.register = async (req, res) => {
 };
 
 module.exports.renderLogin = (req, res) => {
-  res.render("users/login");
+  const randomLink = getRandomLink();
+  res.render("users/login", { randomLink });
 };
 
 module.exports.login = (req, res) => {
