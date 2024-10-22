ab9c578504be47bb06f28a24db4f62e933ddb9ac
Jake Dusing <dusingjake@gmail.com>
2024-10-21 20:18:23 -0500

    add helmet

--- schemas.js
+++ schemas.js
@@ -1,11 +1,35 @@
-const Joi = require("joi");
+const BaseJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value)
+          return helpers.error("string.escapeHTML", { value });
+        return clean;
+      },
+    },
+  },
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.bookSchema = Joi.object({
   book: Joi.object({
-    title: Joi.string().required(),
-    author: Joi.string().required(),
+    title: Joi.string().required().escapeHTML(),
+    author: Joi.string().required().escapeHTML(),
     // image: Joi.string().required(),
-    description: Joi.string().required(),
+    description: Joi.string().required().escapeHTML(),
   }).required(),
   deleteImages: Joi.array(),
 });
@@ -13,6 +37,6 @@ module.exports.bookSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
     rating: Joi.number().required(),
-    body: Joi.string().required(),
+    body: Joi.string().required().escapeHTML(),
   }).required(),
 });
