3d02668007beeb2c8ccebb136c22bfbbc4b9c51a
Jake Dusing <dusingjake@gmail.com>
2024-10-26 19:11:15 -0500

    add profile image upload

--- models/user.js
+++ models/user.js
@@ -2,12 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
+
 const UserSchema = new Schema({
   email: {
     type: String,
     required: true,
     unique: true,
   },
+  profileImage: ImageSchema,
 });
 
 UserSchema.plugin(passportLocalMongoose);
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,9 @@ const catchAsync = require("../utils/catchAsync");
 const User = require("../models/user");
 const users = require("../controllers/users");
 const { storeReturnTo, isLoggedIn } = require("../middleware");
+const multer = require("multer");
+const { storage, cloudinary } = require("../cloudinary");
+const upload = multer({ storage });
 
 // Register routes
 router
@@ -28,9 +31,41 @@ router
 // Logout route
 router.get("/logout", users.logout);
 
-//User Profile route (GET request, only accessible if Logged in)
-//router.get("/profile", isLoggedIn, catchAsync(users.showProfile));
+// Profile Picture upload Route
+router.post(
+  "/upload-profile-picture",
+  isLoggedIn,
+  upload.single("profileImage"),
+  async (req, res) => {
+    try {
+      const user = await User.findById(req.user._id);
 
+      // if the user already has a profile image, delete it from cloudinary
+      if (user.profileImage && user.profileImage.filename) {
+        await cloudinary.uploader.destroy(user.profileImage.filename); // Delete the old profile image
+      }
+
+      // upload the new image to CLoudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+
+      // upload the user's profile picture
+      user.profileImage = {
+        url: result.secure_url,
+        filename: result.public_id,
+      };
+
+      await user.save();
+      req.flash("success", "Profile picture updated successfully!");
+      res.redirect(`/users/${req.user._id}`);
+    } catch (error) {
+      console.log(error);
+      req.flash("error", "Failed to upload profile picture.");
+      res.redirect(`/users/${req.user._id}`);
+    }
+  }
+);
+
+// Show user profile route
 router.get("/users/:id", isLoggedIn, catchAsync(users.showProfile));
 
 module.exports = router;
