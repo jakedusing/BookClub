34a03e2c272adc30cd519a967bf7e4abd82fe0c4
Jake Dusing <dusingjake@gmail.com>
2024-10-28 18:53:26 -0500

    start set up for mongo atlas and update styling

--- app.js
+++ app.js
@@ -21,7 +21,12 @@ const userRoutes = require("./routes/users");
 const bookRoutes = require("./routes/books");
 const reviewRoutes = require("./routes/reviews");
 
-mongoose.connect("mongodb://localhost:27017/bookclub");
+const MongoDBStore = require("connect-mongo")(session);
+
+// const dbURL = process.env.DB_URL;
+// "mongodb://localhost:27017/bookclub"
+const dbUrl = "mongodb://localhost:27017/bookclub";
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -43,7 +48,18 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(mongoSanitize());
 
+const store = new MongoDBStore({
+  url: dbUrl,
+  secret: "thisshouldbeabettersecret!",
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e);
+});
+
 const sessionConfig = {
+  store,
   name: "session",
   secret: "thisshouldbeabettersecret!",
   resave: false,
